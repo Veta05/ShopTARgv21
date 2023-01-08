aa473dcf646a39e52bcb1693fd6c285a18dd3be4
veta05 <jelizaveta05@gmail.com>
2023-01-08 13:10:57 +0200

    add car model

---
+++ ShopTARgv21/Models/Car/CarListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopTARgv21.Models.Car
+{
+    public class CarListViewModel : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ShopTARgv21/Models/Car/CarViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopTARgv21.Models.Car
+{
+    public class CarViewModel : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
