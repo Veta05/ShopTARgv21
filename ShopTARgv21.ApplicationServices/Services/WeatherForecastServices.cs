77d05eb7585a16fd692c67695a65ebf3757ab80c
veta05 <jelizaveta05@gmail.com>
2023-03-06 09:42:10 +0200

    finish weather forecast

--- ShopTARgv21.ApplicationServices/Services/WeatherForecastServices.cs
+++ ShopTARgv21.ApplicationServices/Services/WeatherForecastServices.cs
@@ -9,31 +9,33 @@ namespace ShopTARgv21.ApplicationServices.Services
         public async Task<WeatherResultDto> WeatherDetail(WeatherResultDto dto)
         {
             string apiKey = "1Aw4wIUG3AlDUZpDWLpbb0yFB4P0qD3N";
-            var url = $"http://dataservice.accuweather.com/currentconditions/v1/1?apikey={apiKey}&et&details=false";
+            //var url = $"http://dataservice.accuweather.com/currentconditions/v1/1?apikey={apiKey}&et&details=false";
             var url2 = $"http://dataservice.accuweather.com/currentconditions/v1/1?apikey=1Aw4wIUG3AlDUZpDWLpbb0yFB4P0qD3N&et&details=false";
 
             using (WebClient client = new WebClient())
             {
+
+                List<DailyForecast> weatherInfo = new();
                 string json = client.DownloadString(url2);
                 //ainult ühe classi saab deserialiseerida korraga
-                Root weatherInfo = (new JavaScriptSerializer()).Deserialize<Root>(json);
+                weatherInfo = new JavaScriptSerializer().Deserialize<List<DailyForecast>>(json);
 
-                dto.LocalObservationDateTime = weatherInfo.DailyForecasts[0].LocalObservationDateTime;
-                dto.EpochTime = weatherInfo.DailyForecasts[0].EpochTime;
-                dto.WeatherText = weatherInfo.DailyForecasts[0].WeatherText;
-                dto.WeatherIcon = weatherInfo.DailyForecasts[0].WeatherIcon;
-                dto.HasPrecipitation = weatherInfo.DailyForecasts[0].HasPrecipitation;
-                dto.IsDayTime = weatherInfo.DailyForecasts[0].IsDayTime;
-                dto.MobileLink = weatherInfo.DailyForecasts[0].MobileLink;
-                dto.Link = weatherInfo.DailyForecasts[0].Link;
+                dto.LocalObservationDateTime = weatherInfo[0].LocalObservationDateTime;
+                dto.EpochTime = weatherInfo[0].EpochTime;
+                dto.WeatherText = weatherInfo[0].WeatherText;
+                dto.WeatherIcon = weatherInfo[0].WeatherIcon;
+                dto.HasPrecipitation = weatherInfo[0].HasPrecipitation;
+                dto.IsDayTime = weatherInfo[0].IsDayTime;
+                dto.MobileLink = weatherInfo[0].MobileLink;
+                dto.Link = weatherInfo[0].Link;
 
-                dto.TempMetricValue = weatherInfo.DailyForecasts[0].Temperature.Metric.Value;
-                dto.TempMetricUnit = weatherInfo.DailyForecasts[0].Temperature.Metric.Unit;
-                dto.TempMetricUnitType = weatherInfo.DailyForecasts[0].Temperature.Metric.UnitType;
+                dto.TempMetricValue = weatherInfo[0].Temperature.Metric.Value;
+                dto.TempMetricUnit = weatherInfo[0].Temperature.Metric.Unit;
+                dto.TempMetricUnitType = weatherInfo[0].Temperature.Metric.UnitType;
 
-                dto.TempImperialValue = weatherInfo.DailyForecasts[0].Temperature.Imperial.Value;
-                dto.TempImperialUnit = weatherInfo.DailyForecasts[0].Temperature.Imperial.Unit;
-                dto.TempImperialUnitType = weatherInfo.DailyForecasts[0].Temperature.Imperial.UnitType;
+                dto.TempImperialValue = weatherInfo[0].Temperature.Imperial.Value;
+                dto.TempImperialUnit = weatherInfo[0].Temperature.Imperial.Unit;
+                dto.TempImperialUnitType = weatherInfo[0].Temperature.Imperial.UnitType;
 
                 var jsonString = new JavaScriptSerializer().Serialize(dto);
             }
